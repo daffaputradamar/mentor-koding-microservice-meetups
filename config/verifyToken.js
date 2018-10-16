1ca1fd6e8f256c7201d5bf95a772bfd284dc74ba
daffadwiputra <daffaputradamar@gmail.com>
2018-10-17 06:31:53 +0700

    delete default token

--- config/verifyToken.js
+++ config/verifyToken.js
@@ -8,9 +8,9 @@ function verifyToken(req, res, next) {
 
     next();
   } else {
-    // req.token = "";
-    req.token =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YmM1ZWRiMWI4ZDg2NzM1ZThlMzdlYjYiLCJpYXQiOjE1Mzk3MzE1OTAsImV4cCI6MTUzOTgxNzk5MH0.F4xoyWSKxtrdmjOlRrnu0oPB_n3y40VbYLd17LNrq2Y";
+    req.token = "";
+    // req.token =
+    //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YmM1ZWRiMWI4ZDg2NzM1ZThlMzdlYjYiLCJpYXQiOjE1Mzk3MzE1OTAsImV4cCI6MTUzOTgxNzk5MH0.F4xoyWSKxtrdmjOlRrnu0oPB_n3y40VbYLd17LNrq2Y";
     next();
   }
 }
